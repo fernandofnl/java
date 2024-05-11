7dac7b34e7651d68af7883adea83833b326bba11
fernandofnl <fernandofnl@gmail.com>
2024-05-11 14:32:16 -0300

    inicio estudo java

---
+++ logica-de-programacao/src/tipos/primitivos/Inicio.java
@@ -0,0 +1,15 @@
+package tipos.primitivos;
+
+public class Inicio {
+
+    public static void main(String[] args) {
+
+        System.out.println("Bom dia, garotinho!!");
+        
+
+
+
+
+
+    }
+}
